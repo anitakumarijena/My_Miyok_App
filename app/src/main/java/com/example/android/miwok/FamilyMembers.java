75badc4243d331ae31d107f0884ec44661a7d19e
anitakumarijena <anitakumari1408@gmail.com>
2020-06-28 23:10:36 +0530

    slight change in comment by replacing activity_numbers.xml to word_list.xml

--- app/src/main/java/com/example/android/miwok/FamilyMembers.java
+++ app/src/main/java/com/example/android/miwok/FamilyMembers.java
@@ -56,7 +56,7 @@ public class FamilyMembers extends AppCompatActivity {
 
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
-        // activity_numbers.xml layout file.
+        // word_list.xml layout file.
         ListView listView = (ListView) findViewById(R.id.list);
 
         // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
